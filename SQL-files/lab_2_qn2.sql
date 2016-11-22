ee9d3b15d6dadafbf5137272ef66a6da18785059
Deddy T <deddybuster@hotmail.com>
2016-11-22 22:44:57 +0800

    Updated lab 2 qn 2

--- SQL-files/lab_2_qn2.sql
+++ SQL-files/lab_2_qn2.sql
@@ -6,9 +6,10 @@ SELECT ename, age FROM Employee, Department WHERE eid = managerid;
 SELECT ename, salary FROM Employee, Works WHERE Employee.eid = Works.eid AND did = 'HR';
 
 --Qn 2.3
-SELECT did AS 'Department No', COUNT(eid) AS 'Total Employees' FROM Works WHERE did = 'Finance' GROUP BY did;
+--SELECT did AS 'Department No', COUNT(eid) AS 'Total Employees' FROM Works WHERE did = 'Finance' GROUP BY did;
+SELECT did AS 'Department No', COUNT(*) AS 'Total Employees' FROM Works GROUP BY did HAVING COUNT(*) > 5;
 
 --Qn 2.4
-SELECT managerid, budget AS 'Total Budget' FROM Department, Works
-WHERE Department.did = Works.did AND budget >= 5000000 
-GROUP BY managerid, budget
+SELECT managerid, SUM(budget) AS 'Total Budget' FROM Department
+HAVING SUM(budget) >= 5000000 
+GROUP BY managerid;
